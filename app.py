f4f95276609f48d1490f13ad108b4aa63b79a1a7
JoePeacock <jpeacockinc@gmail.com>
2013-09-21 17:35:37 -0400

    added front-end

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ app = Flask(__name__)
 app.debug = True
 
 clients = []
-listActive = [{"name": "Hello"}, {"name":"shit"}]
+listActive = [{"name": "Hello"}]
 listAvailable = [{"name": "YOLO"}, {"name":"SHIT"}]
+listInUse = [{"name": "Inactive"}, {"name": "Joe Peacock"}]
 lanIp = ""
 
 @app.before_request
@@ -23,7 +24,7 @@ def connect():
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html', listActive=listActive, listAvailable=listAvailable)
+    return flask.render_template('index.html', listActive=listActive, listAvailable=listAvailable, device={"name": "Peacock", "ip": "10.0.0.4"}, listInUse=listInUse)
 
 def set_lan_ip():
     lanIp = commands.getoutput("/sbin/ifconfig").split("\n")[10].split(" ")[1]
