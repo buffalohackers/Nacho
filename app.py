d5f69d2b2f6558fde2ac86c97901fffd30aedae1
JoePeacock <jpeacockinc@gmail.com>
2013-09-21 16:15:47 -0400

    added templates

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 from flask import Flask
+import flask
 import json
+import socket
+import os
+import commands
 
 app = Flask(__name__)
 app.debug = True
 
 clients = []
+listActive = [{"name": "Hello"}, {"name":"shit"}]
+listAvailable = [{"name": "YOLO"}, {"name":"SHIT"}]
+lanIp = ""
+
+@app.before_request
+def before_request():
+    set_lan_ip()
 
 @app.route('/connect')
 def connect():
     return "hello World"
 
 @app.route('/')
-def helo():
-    return "hello world"
+def index():
+    return flask.render_template('index.html', listActive=listActive, listAvailable=listAvailable)
+
+def set_lan_ip():
+    lanIp = commands.getoutput("/sbin/ifconfig").split("\n")[10].split(" ")[1]
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=1337)
